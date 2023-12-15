f7ac16ceb3b4a3986ea5aa97d40c9051644bbeff
nicolas riera <nicolariera@gmail.com>
2023-12-15 15:20:25 +0100

    create function validate

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -28,3 +28,65 @@ function launchModal() {
 function closeModal() {
   modalbg.style.display = "none";
 }
+
+// DOM Elements
+
+function validate() {
+  // inputs
+  let prenom = document.getElementById("first").value;
+  console.log(prenom, "prenom");
+  let nom = document.getElementById("last").value;
+  console.log(nom, "nom");
+  let email = document.getElementById("email").value;
+  console.log(email, "email");
+  let birthdate = document.getElementById("birthdate").value;
+  console.log(birthdate, "birthdate");
+  let quantity = document.getElementById("quantity").value;
+  console.log(quantity, "quantity");
+
+  // radio buttons
+  let locationChoice = document.getElementsByName("location");
+  let choice = "";
+  for (let i = 0; i < locationChoice.length; i++) {
+    if (locationChoice[i].checked) {
+      choice = locationChoice[i].value;
+      break;
+    }
+  }
+  console.log(choice, "choice");
+
+  // checkbox
+  let checkbox1 = document.getElementById("checkbox1").checked;
+  let checkbox2 = document.getElementById("checkbox2").checked;
+
+  let checkboxSelection = [checkbox1, checkbox2];
+  let checkbox = "";
+  for (let i = 0; i < checkboxSelection.length; i++) {
+    if (checkboxSelection[i]) {
+      checkbox = checkboxSelection[i];
+      break;
+    }
+  }
+  console.log(checkbox, "checkbox");
+
+  // Effectuer la validation
+  if (
+    prenom.length >= 2 &&
+    nom.length >= 2 &&
+    validateEmail(email) &&
+    !isNaN(quantity) &&
+    choice &&
+    checkbox
+  ) {
+    alert("Formulaire valide. Soumission réussie!");
+  } else {
+    alert("Veuillez remplir correctement tous les champs du formulaire.");
+  }
+}
+
+function validateEmail(email) {
+  // Expression régulière pour la validation de l'adresse électronique
+  var re = /\S+@\S+\.\S+/;
+  return re.test(email);
+}
+console.log(prenom, "prenom");
