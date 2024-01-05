8e823aa6a2fc707b111908fb5adb3bfc3cf1eb43
nicolas riera <nicolariera@gmail.com>
2024-01-05 16:37:40 +0100

    added validation and try catch

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,10 +1,6 @@
 function editNav() {
   var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
+  x.classList.toggle("responsive");
 }
 
 // DOM Elements
@@ -29,64 +25,117 @@ function closeModal() {
   modalbg.style.display = "none";
 }
 
-// DOM Elements
+// FORMS
 
-function validate() {
-  // inputs
-  let prenom = document.getElementById("first").value;
-  console.log(prenom, "prenom");
-  let nom = document.getElementById("last").value;
-  console.log(nom, "nom");
-  let email = document.getElementById("email").value;
-  console.log(email, "email");
-  let birthdate = document.getElementById("birthdate").value;
-  console.log(birthdate, "birthdate");
-  let quantity = document.getElementById("quantity").value;
-  console.log(quantity, "quantity");
+function affihcerMessageErreur(message) {
+  let formData = document.querySelector(".formData");
+  let messageErreur = document.getElementById("messageErreur");
 
-  // radio buttons
-  let locationChoice = document.getElementsByName("location");
-  let choice = "";
-  for (let i = 0; i < locationChoice.length; i++) {
-    if (locationChoice[i].checked) {
-      choice = locationChoice[i].value;
-      break;
-    }
+  if (!messageErreur) {
+    messageErreur = document.createElement("span");
+    messageErreur.id = "messageErreur";
+    messageErreur = message;
+    formData.append(messageErreur);
   }
-  console.log(choice, "choice");
+  messageErreur.innerText = message;
+}
 
-  // checkbox
-  let checkbox1 = document.getElementById("checkbox1").checked;
-  let checkbox2 = document.getElementById("checkbox2").checked;
+function gererFormulaire() {
+  try {
+    let inputPrenom = document.getElementById("first").value.trim();
+    verifierChamp(inputPrenom);
 
-  let checkboxSelection = [checkbox1, checkbox2];
-  let checkbox = "";
-  for (let i = 0; i < checkboxSelection.length; i++) {
-    if (checkboxSelection[i]) {
-      checkbox = checkboxSelection[i];
-      break;
+    let inputNom = document.getElementById("last").value.trim();
+    verifierChampNom(inputNom);
+
+    let inputEmail = document.getElementById("email").value.trim();
+    validateEmail(inputEmail);
+
+    let inputBirthdate = document.getElementById("birthdate").value.trim();
+    verifierChampBirthdate(inputBirthdate);
+
+    let inputQuantity = document.getElementById("quantity").value.trim();
+    verifierChamp(inputQuantity);
+
+    let checkboxLocation = document.querySelectorAll('input[name="location"]');
+    let location = "";
+    for (let index = 0; index < checkboxLocation.length; index++) {
+      if (checkboxLocation[index].checked) {
+        location = checkboxLocation[index].value;
+        break;
+      }
     }
-  }
-  console.log(checkbox, "checkbox");
+    verifierCheckbox(location);
+    affihcerMessageErreur("");
 
-  // Effectuer la validation
-  if (
-    prenom.length >= 2 &&
-    nom.length >= 2 &&
-    validateEmail(email) &&
-    !isNaN(quantity) &&
-    choice &&
-    checkbox
-  ) {
-    alert("Formulaire valide. Soumission réussie!");
-  } else {
-    alert("Veuillez remplir correctement tous les champs du formulaire.");
+    let checkboxCondition = document.getElementById("checkbox1").checked;
+    verifierCondition(checkboxCondition);
+  } catch (erreur) {
+    affihcerMessageErreur(erreur.message);
   }
 }
+// Validation
 
 function validateEmail(email) {
   // Expression régulière pour la validation de l'adresse électronique
-  var re = /\S+@\S+\.\S+/;
-  return re.test(email);
+  let emailRegExp = new RegExp(
+    "^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\\.[a-z]{2,4}$"
+  );
+  if (!emailRegExp.test(email)) {
+    throw new Error("Vous devez entrer une adresse email valide.");
+  }
 }
-console.log(prenom, "prenom");
+
+// vérification des champs à l'envoi du formulaire
+
+function verifierChamp(champ) {
+  // Si le champ est vide, on lance une exception
+  if (champ === "") {
+    throw new Error(`Vous devez remplir le champ.`);
+  }
+}
+
+function verifierChampNom(nom) {
+  // Si le nom a moins de deux caractères, on lance une exception
+  if (nom.length < 2) {
+    throw new Error(
+      "Vous devez entrer 2 caractères ou plus pour le champ du nom."
+    );
+  }
+}
+
+function verifierChampBirthdate(birthdate) {
+  // Si le birthdate est vide, on lance une exception
+  if (birthdate === "jj/mm/aaaa") {
+    throw new Error("Vous devez entrer votre date de naissance.");
+  }
+}
+
+function verifierCheckbox(checkbox) {
+  // Si la checkbox est vide, on lance une exception
+  if (checkbox === "") {
+    throw new Error("Vous devez choisir une option.");
+  }
+}
+
+function verifierCondition(condition) {
+  // Si la condition est vide, on lance une exception
+  if (condition === false) {
+    throw new Error(
+      "Vous devez vérifier que vous acceptez les termes et conditions."
+    );
+  }
+}
+
+let form = document.querySelector("form");
+
+form.addEventListener("submit", (event) => {
+  try {
+    event.preventDefault();
+    gererFormulaire();
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// Vérifiaction des champs à la saisie de l'utilisateur
